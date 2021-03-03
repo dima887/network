909b27deb69dfcceaadb22eec569634c0d7087e2
dima887 <58649137+dima887@users.noreply.github.com>
2021-03-03 10:56:46 +0300

    Update index.php

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -1,3 +1,36 @@
+<?php if (!empty($_POST) and $_POST['type'] == 2): ?>
+    <h1 class="my-4">Истории по запросу "<?=$_POST['search']?>"</h1>
+    <?php if (!empty($arr)): ?>
+
+        <?php foreach ($arr as $value): ?>
+            <div class="card mb-4">
+                <?php if ($value['idmedia'] == 2): ?>
+
+                    <video controls src="<?=$value['path'] ?>"></video>
+
+                <?php endif; ?>
+
+                <?php if ($value['idmedia'] == 1): ?>
+
+                    <img class="card-img-top" src="<?=$value['path'] ?>" alt="Card image cap">
+
+                <?php endif; ?>
+                <div class="card-body">
+                    <h2 class="card-title"><?=$value['name'] ?></h2>
+                    <p class="card-text"><?=$value['story'] ?></p>
+                </div>
+                <div class="card-footer text-muted">
+                    <span class="float-left"><?=$value['city'] ?></span>
+                    <span class="float-right"><?=$value['datatime'] ?></span>
+                </div>
+            </div>
+
+        <?php endforeach; ?>
+    <?php else: ?>
+        <h3>Историй не найдено...</h3>
+    <?php endif; ?>
+<?php else: ?>
+
 <h1 class="my-4">Все истории</h1>
 
 <?php if (!empty($mainStory)): ?>
@@ -37,6 +70,7 @@
 </div>
 <?php else: ?>
     <h3>Историй пока нет...</h3>
+<?php endif; ?>
 <?php endif; ?>
 
 <!--<script src="/js/story.js"></script>-->
